59d5ead00e4d8cddeec2985b42a10834ac6cbbab
akpekig <gillianakpeki@hotmail.co.uk>
2023-06-25 23:16:03 +0100

    Add heading transition

--- src/components/frame.js
+++ src/components/frame.js
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import * as React from 'react';
 import Particles from 'react-tsparticles';
 import resolveConfig from 'tailwindcss/resolveConfig';
@@ -199,26 +200,29 @@ const Frame = (props) => {
     detectRetina: true,
   };
   return (
-    <div className="relative w-screen min-h-screen h-100 bg-gradient-to-b from-ink-100 to-ink-200 bg-[length:400%_400%] pb-96 overflow-auto">
+    <div className="relative w-screen min-h-screen h-100 bg-gradient-to-b from-ink-100 to-ink-200 bg-[length:400%_400%] overflow-auto">
       <Particles
         id="tsparticles"
         init={particlesInit}
         loaded={particlesLoaded}
         options={particlesOpts}
       />
-      <div className="relative grid sm:grid-cols-3 px-12">
-        <header className="col-span-full -rotate-3 -skew-y-3 sm:scale-[2] my-32 mx-auto">
-          <a href="/" className="font-bold select-none cursor-pointer">
-            <h1 className="text-stroke-gold font-heading text-[2rem]/[0.675]">
+      <div className="relative flex px-12">
+        <motion.header 
+          initial={{ y: '350%' }}
+          animate={{ y: '100%' }}
+          transition={{ ease: [0, 0.71, 0.37, 1], duration: 0.5 }}
+          className="col-span-full mx-auto font-bold select-none"
+        >
+            <h1 className="text-stroke-gold font-heading leading-6 heading">
               Web
               <br />
               Developer
             </h1>
-            <h2 className="text-gold font-subheading text-4xl/[0.5] text-right">
+            <h2 className="mt-5 text-gold font-subheading text-right heading">
               Gillian A
             </h2>
-          </a>
-        </header>
+        </motion.header>
         <main className="col-span-full sm:col-start-1 sm:col-end-3 md:col-start-2 text-plum font-body text-xl">
           {props.children}
         </main>
